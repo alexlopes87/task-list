fa0343bf7dd73b887a246524c5ae127b48478a7e
alexlopes87 <172157268+alexlopes87@users.noreply.github.com>
2024-08-11 12:44:58 -0300

    update bug fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 function addTarefa() {
-    const tarefa = document.getElementById('tarefa').value;
+    const tarefa = document.getElementById('tarefa').value.trim();
     if (tarefa == '') {
         alert('Por favor, digite um tarefa!')
     } else {
-        const tarefa = document.getElementById('tarefa').value;
         const lista = document.getElementById('lista-tarefas');
-        const item = document.createElement('li');
-        item.textContent = tarefa;
-        item.addEventListener('click', function(){
-            item.style.textDecoration = 'line-through';
-            item.style.color = 'gray';
-        });
-        item.addEventListener('dblclick', function(){
-            item.style.textDecoration = 'none';
-            item.style.color = 'black';
-        });
-        lista.appendChild(item);
-        document.getElementById('tarefa').value = '';
+        const cont = lista.getElementsByTagName('li').length;
+
+        if (cont < 10) {
+            const item = document.createElement('li');
+            item.textContent = tarefa;
+
+            // Criar botão de exclusão
+            const btnExcluir = document.createElement('button');
+            btnExcluir.textContent = 'Excluir';
+            btnExcluir.style.marginLeft = '10px'; // Adiciona um espaço entre o texto e o botão
+            btnExcluir.addEventListener('click', function(){
+                lista.removeChild(item);
+                cont-=1;
+            });
+
+            // Adicionar botão de exclusão ao item
+            item.appendChild(btnExcluir);
+
+            item.addEventListener('click', function(){
+                item.style.textDecoration = 'line-through';
+                item.style.color = 'gray';
+            });
+            item.addEventListener('dblclick', function(){
+                item.style.textDecoration = 'none';
+                item.style.color = 'black';
+            });
+            lista.appendChild(item);
+            document.getElementById('tarefa').value = '';
+
+            cont+=1;
+        } else {
+            alert('Número máximo de tarefas atingido!');
+
+        }
     }
 }
