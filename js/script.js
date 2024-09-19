313642e704584e9601f468ce12aa2c4ef31fc9db
alexlopes87 <172157268+alexlopes87@users.noreply.github.com>
2024-09-19 18:40:22 -0300

    update-190920241840

--- js/script.js
+++ js/script.js
@@ -3,36 +3,44 @@ function addTarefa() {
     if (tarefa != '') {
         const lista = document.getElementById('lista-tarefas');
         const contador = lista.getElementsByTagName('li').length;
-        if (contador < 10) {
+        if (contador < 20) {
             const item = document.createElement('li');
-            item.textContent = tarefa;
-            lista.appendChild(item);
-            //document.getElementById('tarefa').value = '';
+            const span = document.createElement('span');
+            span.textContent = `${contador + 1}. ${tarefa}`;
+            item.appendChild(span);
 
             //Criar checkbox
             const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox'
+            checkbox.type = 'checkbox';
+            checkbox.title = 'Riscar';
             checkbox.addEventListener('change', function() {
                 if (checkbox.checked) {
-                    item.style.textDecoration = 'line-through';
-                    item.style.color = 'gray';
+                    span.style.textDecoration = 'line-through';
+                    span.style.color = 'gray';
                 } else {
-                    item.style.textDecoration = 'none';
-                    item.style.color = 'black';
+                    span.style.textDecoration = 'none';
+                    span.style.color = 'black';
                 }
             });
             item.appendChild(checkbox);
             
             //Criar botão de excluir
             const btnExcluir = document.createElement('button');
-            btnExcluir.textContent = 'Del';
-            btnExcluir.id = 'btnExcluir'
+            //btnExcluir.textContent = 'Del';
+            btnExcluir.innerHTML = '&#128465;';
+            btnExcluir.id = 'btnExcluir';
+            btnExcluir.title = 'Excluir';
             btnExcluir.addEventListener('click', function() {
-                lista.removeChild(item);
+                if (confirm('Tem certeza que deseja excluir a tarefa?')) {
+                    lista.removeChild(item);
+                }
             });
             item.appendChild(btnExcluir);
+
+            lista.appendChild(item)
+            document.getElementById('tarefa').value = '';
         } else {
-            alert('Você atingiu o limite de tarefas!');
+            alert('Você atingiu o limite de 20 tarefas!');
         }
     } else {
         alert('Por favor, digite uma tarefa!');
